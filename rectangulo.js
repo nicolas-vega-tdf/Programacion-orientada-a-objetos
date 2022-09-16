b16f6ba73830520dd3baab4fbe16666d80896af8
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2022-09-16 13:35:00 -0300

    ejercicio terminado

--- rectangulo.js
+++ rectangulo.js
@@ -1,20 +1,20 @@
-var rectanculo = /** @class */ (function () {
-    function rectanculo(paramBase, paramAltura) {
+var rectangulo = /** @class */ (function () {
+    function rectangulo(paramBase, paramAltura) {
         this.base = paramBase;
         this.altura = paramAltura;
     }
-    rectanculo.prototype.getBase = function () {
+    rectangulo.prototype.getBase = function () {
         return this.base;
     };
-    rectanculo.prototype.getAltura = function () {
+    rectangulo.prototype.getAltura = function () {
         return this.altura;
     };
-    rectanculo.prototype.setArea = function () {
+    rectangulo.prototype.setArea = function () {
         var area = 0;
         area = this.base * this.altura;
         return area;
     };
-    rectanculo.prototype.comparacion = function (rectanculo) {
+    rectangulo.prototype.comparacion = function (rectanculo) {
         if (rectanculo > this.base) {
             return 1;
         }
@@ -25,7 +25,7 @@ var rectanculo = /** @class */ (function () {
             return 0;
         }
     };
-    rectanculo.prototype.comprobacion = function () {
+    rectangulo.prototype.comprobacion = function () {
         if (this.base === this.altura) {
             console.log("es un cuadrado");
         }
@@ -33,7 +33,7 @@ var rectanculo = /** @class */ (function () {
             console.log("no es un cuadrado");
         }
     };
-    rectanculo.prototype.acostado = function () {
+    rectangulo.prototype.acostado = function () {
         if (this.altura > this.base) {
             console.log("esta parado");
         }
@@ -41,12 +41,12 @@ var rectanculo = /** @class */ (function () {
             console.log("esta acostado");
         }
     };
-    return rectanculo;
+    return rectangulo;
 }());
-var hola = new rectanculo(250, 150);
-//console.log(hola.getAltura())
-//console.log(hola.getBase())
-//console.log(hola.setArea())
-//console.log(hola.comparacion(50))
-//hola.comprobacion()
-hola.acostado();
+var consultar = new rectangulo(50, 150);
+consultar.getAltura();
+consultar.getBase();
+consultar.setArea();
+consultar.comparacion(50);
+consultar.comprobacion();
+consultar.acostado();
