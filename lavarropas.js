604cbd83f778f8d74b701cf6ff7a5bdbd24deeda
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2022-09-18 15:57:26 -0300

    añadi geters y seters

--- lavarropas.js
+++ lavarropas.js
@@ -4,27 +4,32 @@ var lavarropas = /** @class */ (function () {
         this.estaPrendido = estaPrendido;
         this.programas = programas;
     }
-    lavarropas.prototype.prendido = function () {
-        if (this.estaPrendido) {
+    lavarropas.prototype.getLavar = function () {
+        return this.lavar;
+    };
+    lavarropas.prototype.setLavar = function (plavar) {
+        this.lavar = plavar;
+    };
+    lavarropas.prototype.getPrendido = function () {
+        return this.estaPrendido;
+    };
+    lavarropas.prototype.setPrendido = function (pPrendido) {
+        if (pPrendido === false) {
             this.estaPrendido = false;
         }
         else {
             this.estaPrendido = true;
         }
     };
-    lavarropas.prototype.pausa = function () {
+    lavarropas.prototype.setPausa = function () {
         console.log("Pulse para reanudar");
     };
-    lavarropas.prototype.apagado = function () {
-        if (this.estaPrendido) {
-            this.estaPrendido = true;
-        }
-        else {
-            this.estaPrendido = false;
-        }
+    lavarropas.prototype.getProgramas = function () {
+        return this.programas;
+    };
+    lavarropas.prototype.setProgramas = function (pProgramas) {
+        this.programas = pProgramas;
     };
     return lavarropas;
 }());
 var lavarropa = new lavarropas(true, true, "centrifugar");
-lavarropa.prendido();
-lavarropa.apagado();
